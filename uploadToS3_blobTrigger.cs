d05d86c787c64e4943797c6e614cd1d6b8f028c5
Najib Zarrari <nzarrari@microsoft.com>
2021-01-20 11:29:43 -0500

    clean-up

--- uploadToS3_blobTrigger.cs
+++ uploadToS3_blobTrigger.cs
@@ -1,56 +1,44 @@
-// Default URL for triggering event grid function in the local environment.
-// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 using System;
+using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Azure.EventGrid.Models;
-using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
-using System.IO;
-using System.Linq;
 using Amazon;
 using Amazon.S3;
 using Amazon.S3.Transfer;
 
 namespace poceventgrid
 {
-    public static class upload2S3
+    public static class uploadToS3_blobTrigger
     {
-        
         private const string bucketName = "nzbucket123";
         // Specify your bucket region (an example region is shown).
         private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast1;
         private static IAmazonS3 s3Client;
         private static string s3Key = System.Environment.GetEnvironmentVariable("s3Key");
         private static string s3Secret = System.Environment.GetEnvironmentVariable("s3Secret");
-        
 
-        [FunctionName("upload2S3")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, 
-        [Blob("{data.url}", FileAccess.Read, Connection = "StorageConnectionAppSetting")] Stream myBlob, 
-         ILogger log)
+        [FunctionName("uploadToS3_blobTrigger")]
+        public static void Run([BlobTrigger("addresses/{name}", Connection = "StorageConnectionAppSetting2")]Stream myBlob, 
+            string name, 
+            ILogger log)
         {
-            var filename = eventGridEvent.Subject.Split('/').Last();
-            log.LogInformation($"file name: {filename}");
-
-            if(myBlob != null){
-                log.LogInformation($"BlobInput processed Size: {myBlob.Length} bytes");
-            }else{
-                log.LogInformation("blob is null!!!");
-            }
-
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
             log.LogInformation("Upload started");
-            uploadtoS3(myBlob, log, filename);
+            uploadtoS3(myBlob, log, name);
             log.LogInformation("upload ended!!");
-        }
 
-        private static void uploadtoS3(Stream fileToUpload, ILogger log, string filename){         
+        }
+        private static void uploadtoS3(Stream fileToUpload, ILogger log, string filename){       
+
+            
             var awsCredentials = new Amazon.Runtime.BasicAWSCredentials(s3Key, s3Secret);   
+
             s3Client = new AmazonS3Client(awsCredentials, bucketRegion);
             try
             {
                 var fileTransferUtility = new TransferUtility(s3Client);
-                fileTransferUtility.UploadAsync(fileToUpload, bucketName, filename).Wait();
+                fileTransferUtility.UploadAsync(fileToUpload, bucketName, filename);
                 log.LogInformation("Upload 3 completed");            
             }
             catch (AmazonS3Exception e)
@@ -62,5 +50,6 @@ namespace poceventgrid
                 log.LogInformation("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
             }
         }
+
     }
 }
